8a5b7ae8cc8a115cfe4f341654ab8dff2278cc1b
Chad Nelson <chad.nelson@temple.edu>
2015-11-13 16:32:33 -0500

    Ad rake task

--- Rakefile
+++ Rakefile
@@ -1,6 +1,17 @@
 require "bundler/gem_tasks"
 require "rspec/core/rake_task"
+require 'cdm_batch'
 
 RSpec::Core::RakeTask.new(:spec)
 
 task :default => :spec
+
+task :upload_etd, [:etd_metadata_path] do |t, args|
+  form_config_path = File.join('config', "etd_form.yml")
+  creds = CdmBatch.get_cdm_credentials
+  filler = CdmBatch::FormFiller.new args.etd_metadata_path, form_config_path, creds
+  filler.upload_batch
+
+end
+
+
--- lib/cdm_batch.rb
+++ lib/cdm_batch.rb
@@ -2,8 +2,12 @@ require "cdm_batch/version"
 require "cdm_batch/etd_loader"
 require "cdm_batch/form"
 require "cdm_batch/form_filler"
+require "cdm_batch/logger"
+require "cdm_batch/failed_etd_writer"
+require "yaml"
 
 module CdmBatch
+
 
   def self.header_to_symbol(header)
     header.downcase
@@ -11,6 +15,36 @@ module CdmBatch
       .gsub("-","_")
       .to_sym 
   end
+
+  def self.get_cdm_credentials
+  	
+  	creds = get_creds_from_yaml
+  	unless creds
+      puts "ContentDM username"  
+      STDOUT.flush  
+      username = STDIN.gets.chomp
+      
+      puts "Password"  
+      STDOUT.flush  
+      password = STDIN.gets.chomp
+
+      creds = {:username => username, :password => password}
+    end
+    creds
+  end
+
+  def self.get_creds_from_yaml(filename=nil)
+  	filename ||= ".cdm_creds"
+  	if File.exists?(filename)
+      yaml_creds = File.open(filename) { |file| YAML.load(file) }
+      creds = {:username => yaml_creds["username"], :password => yaml_creds["password"]}
+    else
+      creds =  nil  
+  	end
+    return creds
+  end
+
+
 
 
 end
