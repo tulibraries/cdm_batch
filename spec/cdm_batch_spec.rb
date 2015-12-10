575a0c11e6e3c055ce746ed2b1a445165213104d
Chad Nelson <chad.nelson@temple.edu>
2015-12-10 10:06:55 -0500

    Remove specs now tested elsewhere

--- spec/cdm_batch_spec.rb
+++ spec/cdm_batch_spec.rb
@@ -4,18 +4,4 @@ describe CdmBatch do
   it 'has a version number' do
     expect(CdmBatch::VERSION).not_to be nil
   end
-  
-  it 'checks that each records associated file is present' do
-	  
-	end
-
-  it 'logs each successful upload to current batch log'
-  it 'logs errors to current batch error log'
-  
-  it 'archives current batch log when all items successfully uploaded'
-  it 'archives current batch errors log when all items successfully uploaded'
-
-  it 'does not archive current batch log if any batch item raises an error'
-
-  it 'does not re-upload items in current batch log if batch restarted'  
 end
